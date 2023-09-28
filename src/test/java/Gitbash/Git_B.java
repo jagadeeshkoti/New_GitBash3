53a07d114cdd78adc11bd218036915e8188fc69c
jagadeeshkoti <jagadeeshkoti96@gmail.com>
2023-09-28 16:11:58 +0530

    commit1

---
+++ src/test/java/Gitbash/Git_B.java
@@ -0,0 +1,11 @@
+package Gitbash;
+
+import org.testng.annotations.Test;
+
+public class Git_B {
+	@Test
+	public void test1() {
+		System.out.println("created");
+	}
+
+}
---
+++ src/test/java/Gitbash/package-info.java
@@ -0,0 +1,1 @@
+package Gitbash;
